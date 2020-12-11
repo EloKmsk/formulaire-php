809ab29f9583cf3e687be886d7e31c8c97ec8258
EloKmsk <elokmsk@gmail.com>
2020-12-11 11:07:16 +0100

    Formulaire PHP

---
+++ public/contact.php
@@ -0,0 +1,52 @@
+<?php
+
+use \DateTime;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+require __DIR__.'/vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__.'/templates');
+
+
+$twig = new Environment($loader, [
+    'debug' => true,
+    'strict_variables' => true,
+]);
+
+$twig->addExtension(new DebugExtension());
+
+$errors = [];
+
+if ($_POST) {
+    $minLengthSubject = 3;
+    $maxLengthSubject = 190;
+
+    if (empty($_POST['email'])) {
+        $errors['email'] = 'Merci de ne pas laisser ce champ vide';
+    } elseif (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false)  {
+        $errors['email'] = 'Merci de renseigner un email valide';
+    } elseif (strlen($_POST['email']) > 190) {
+        $errors['email'] = "Merci de renseigner un email dont la longueur est comprise entre {$minLengthSubject} et {$maxLengthSubject} inclus";
+    }
+
+    if (empty($_POST['subject'])) {
+        $errors['subject'] = 'Merci de ne pas laisser ce champ vide';
+    } elseif (strlen($_POST['subject']) < 3 || strlen($_POST['subject']) > 190) {
+        $errors['subject'] = "Merci de renseigner un sujet dont la longueur est comprise entre {$minLengthSubject} et {$maxLengthSubject} inclus";
+    }
+
+    $minLengthMessage = 3;
+    $maxLengthMessage = 1000;
+
+    if (empty($_POST['message'])) {
+        $errors['message'] = 'Merci de ne pas laisser ce champ vide';
+    } elseif (strlen($_POST['message']) < 3 || strlen($_POST['message']) > 1000) {
+        $errors['message'] = "Merci de renseigner un message dont la longueur est comprise entre {$minLengthMessage} et {$maxLengthMessage} inclus";
+    }
+}
+
+echo $twig->render('contact.html.twig', [
+    'errors' => $errors,
+]);
